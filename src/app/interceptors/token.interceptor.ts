15cfb74eeaf75a5e41321293167b87a353d3e1a6
sadique <miraigategroup@gmail.com>
2024-02-21 11:56:19 +0530

    update

--- src/app/interceptors/token.interceptor.ts
+++ src/app/interceptors/token.interceptor.ts
@@ -1,7 +1,7 @@
 import { HttpInterceptorFn } from '@angular/common/http';
 
 export const tokenInterceptor: HttpInterceptorFn = (req, next) => {
-  const token = `Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJDMTA2NyIsImF1ZCI6IjIiLCJzY29wZXMiOlt7ImF1dGhvcml0eSI6IjIifV0sImlzcyI6IklTU1VFUiIsImlhdCI6MTcwODQxNzEzMSwiZXhwIjoxNzA5MjgxMTMxfQ.FMFSIVyle864usgPkOqvgzqbEtZ7eSnQXTk37nd-768`;
+  const token = `Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJDMTA2NyIsImF1ZCI6IjIiLCJzY29wZXMiOlt7ImF1dGhvcml0eSI6IjIifV0sImlzcyI6IklTU1VFUiIsImlhdCI6MTcwODQ5MzE5NSwiZXhwIjoxNzA5MzU3MTk1fQ.MxPX4LIPG8vsYGPvpf4e5GNmdYyI0XxCxgqjWmd4-GA`;
   const cloneReq =req.clone({
     setHeaders :{
       Authorization: token
